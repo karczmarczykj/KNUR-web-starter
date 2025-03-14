bc53ac91c4a1cf9a8d3d389b64331603c12feb75
karczmarczykj <j.s.karczmarczyk@gmail.com>
2025-03-14 16:33:30 +0100

    Fixing

--- e2e/healthcheck.spec.ts
+++ e2e/healthcheck.spec.ts
@@ -9,10 +9,3 @@ test('GET / returns 200', async ({ request }) => {
     expect(responseBody).toEqual({ "message": "Hello, world!" });
 });
 
-test('Redirects HTTP to HTTPS', async ({ request }) => {
-  const response = await request.get('http:localhost:2999');
-  expect(response.status()).toBe(200);
-
-  const responseBody = await response.json();
-  expect(responseBody).toEqual({ "message": "Hello, world!" });
-});
